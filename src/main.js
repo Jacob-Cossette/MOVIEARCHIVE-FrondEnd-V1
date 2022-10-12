a80cc4747e06a31b9fd98e143a12e7b54c59ca56
Jacob <jacobcoss7@gamil.com>
2022-10-12 18:31:13 -0400

    test vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,42 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-loadFonts()
+import App from './App';
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+import Login from './components/Login';
+import Main  from './components/Main';
+
+Vue.use(VueRouter);
+Vue.use(Vuetify, {
+   theme: {
+      primary: '#7957d5',
+   },
+});
+
+const router = new VueRouter({
+   routes: [
+      {
+         path: '/login',
+         component: Login,
+      },
+      {
+         path: '/main',
+         component: Main,
+      }
+   ],
+});
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+   router: router,
+   el: '#app',
+   components: { App },
+   template: '<App/>',
+});
+
+router.replace('/login');

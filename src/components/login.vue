0f0e893e417da43ed6fc4fdc35905a87d0c62a6d
Jacob <jacobcoss7@gamil.com>
2022-10-12 18:14:14 -0400

    Add login page with visual frame

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,47 +1,49 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      label="Do you agree?"
-    ></v-checkbox>
-
-    <v-btn
-      class="mr-4"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
+  <v-app id="inspire">
+     <v-content>
+        <v-container fluid fill-height>
+           <v-layout align-center justify-center>
+              <v-flex xs12 sm8 md4>
+                 <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                       <v-toolbar-title>Login form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                       <v-form>
+                          <v-text-field
+                             prepend-icon="person"
+                             name="login"
+                             label="Login"
+                             type="text"
+                          ></v-text-field>
+                          <v-text-field
+                             id="password"
+                             prepend-icon="lock"
+                             name="password"
+                             label="Password"
+                             type="password"
+                          ></v-text-field>
+                       </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                       <v-spacer></v-spacer>
+                       <v-btn color="primary" to="/">Login</v-btn>
+                    </v-card-actions>
+                 </v-card>
+              </v-flex>
+           </v-layout>
+        </v-container>
+     </v-content>
+  </v-app>
 </template>
 
 <script>
-  
+export default {
+  name: 'Login',
+  props: {
+     source: String,
+  },
+};
 </script>
+
+<style></style>
